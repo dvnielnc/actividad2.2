0c104adab78dd015f55d0fe0456fffa4c005b2ec
dvnielnc <djopia863.a@gmail.com>
2024-10-03 16:13:50 -0300

    2da versión actividad 2.2

--- app/src/main/java/com/example/actividad22/MainActivity.java
+++ app/src/main/java/com/example/actividad22/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.actividad22;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,34 +13,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnDescargarImagen;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    Button btnDescargarImagen = findViewById(R.id.btnDescargarImagen);
-    ImageView mImageView = findViewById(R.id.mImageView);
 
-    public void onClick(View v) {
-        new Thread(new Runnable() {
+        btnDescargarImagen = findViewById(R.id.btnDescargarImagen);
+        mImageView = findViewById(R.id.mImageView);
+
+        btnDescargarImagen.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                final Bitmap bitmap = loadImageFromNetwork("http://IP.com/image.png");
-                mImageView.post(new Runnable() {
+            public void onClick(View v) {
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        mImageView.setImageBitmap(bitmap);
+                        final Bitmap bitmap = loadImageFromNetwork("https://intecorecoleta.cl/wp-content/uploads/2022/08/programacion-2-e1551291144973.jpg");
+                        mImageView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mImageView.setImageBitmap(bitmap);
+                            }
+                        });
                     }
-                });
+                }).start();
             }
-        }).start();
+        });
+    }
+
+    private Bitmap loadImageFromNetwork(String urlString) {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
